768f5f011bfe0ff3df9204c78541c4eedeb95616
mcberros <mcberros@gmail.com>
2013-08-10 18:59:02 +0200

    Move product specific locales to application. refs #53961425

--- lib/voluntary_classified_advertising/engine.rb
+++ lib/voluntary_classified_advertising/engine.rb
@@ -1,4 +1,5 @@
 module VoluntaryClassifiedAdvertising
   class Engine < ::Rails::Engine
+    config.i18n.load_path += Dir[File.expand_path("../../../config/locales/**/*.{rb,yml}", __FILE__)]
   end
 end
