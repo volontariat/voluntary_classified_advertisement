f6562601fc2573678c14d781a952c31b5aff9e44
vagrant <vagrant@lucid32.hsd1.ca.comcast.net>
2013-10-24 19:42:32 +0200

    Adds new migration from core.

--- dummy/db/schema.rb
+++ dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130830171113) do
+ActiveRecord::Schema.define(:version => 20131023132038) do
 
   create_table "areas", :force => true do |t|
     t.string   "ancestry"
