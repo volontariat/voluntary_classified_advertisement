12ba20547e285988e6fd5c91167884be17534048
vagrant <vagrant@lucid32.hsd1.ca.comcast.net>
2013-10-24 19:41:02 +0200

    Implements candidatures_attributes.

--- app/models/product/classified_advertisement/vacancy.rb
+++ app/models/product/classified_advertisement/vacancy.rb
@@ -1,4 +1,6 @@
 class Product::ClassifiedAdvertisement::Vacancy < ::Vacancy
+  has_many :candidatures, dependent: :destroy, class_name: 'Product::ClassifiedAdvertisement::Candidature', inverse_of: :vacancy
+  
   attr_accessible :task_id, :task
   
   after_initialize :set_defaults
@@ -18,6 +20,45 @@ class Product::ClassifiedAdvertisement::Vacancy < ::Vacancy
 
   validates :task, presence: true
   
+  def candidatures_attributes=(attributes)
+    self.candidatures ||= []
+    
+    attributes.each do |index, candidature_attributes|
+      destroy = candidature_attributes.delete :_destroy
+      
+      if candidature_attributes[:id].present? && destroy.to_i == 1
+        begin; candidature_class.destroy(candidature_attributes[:id]); rescue ActiveRecord::RecordNotFound; end
+        @candidatures.select!{|v| v.id != candidature_attributes[:id]}
+        next
+      end
+      
+      candidature_attributes[:vacancy] = self
+      candidature = nil
+      
+      if candidature_attributes[:id].present?
+        begin; candidature = candidature_class.find(candidature_attributes[:id]); rescue ActiveRecord::RecordNotFound; end
+        
+        candidature_attributes.delete(:id)
+        
+        candidature.attributes = candidature_attributes if candidature.present?
+      else
+        candidature = candidature_class.new(candidature_attributes)
+      end
+      
+      next unless candidature.present?
+      
+      if candidature_attributes[:id].present?
+        self.candidatures.each_with_index do |current_candidature, candidatures_index|
+          if current_candidature.id.to_s == candidature_attributes[:id]
+            self.candidatures[candidatures_index] = candidature
+          end
+        end
+      else
+        self.candidatures << candidature
+      end
+    end
+  end
+  
   def task
     @task ||= task_class.where(id: task_id).first
   end
@@ -32,6 +73,14 @@ class Product::ClassifiedAdvertisement::Vacancy < ::Vacancy
       "#{product.class.name}::Task".constantize rescue Task
     else
       Task
+    end
+  end
+  
+  def candidature_class
+    if product.present?
+      "#{product.class.name}::Candidature".constantize rescue Candidature
+    else
+      Candidature
     end
   end
   
