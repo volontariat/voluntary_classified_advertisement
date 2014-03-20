86deb6243cc7cba05c9d5f312efb36b29d0cc433
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-03-20 14:14:27 +0100

    Add product to voluntary core navigation.

--- lib/voluntary_classified_advertisement/engine.rb
+++ lib/voluntary_classified_advertisement/engine.rb
@@ -1,5 +1,9 @@
 module VoluntaryClassifiedAdvertisement
   class Engine < ::Rails::Engine
     config.i18n.load_path += Dir[File.expand_path("../../../config/locales/**/*.{rb,yml}", __FILE__)]
+    
+    config.to_prepare do
+      Voluntary::Navigation::Base.add_product('classified-advertisement', t('workflow.user.products.classified_advertisement.title'))
+    end
   end
 end
