8f274b21446a4cb17a32ee6713501a3707dc5f21
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-10-29 18:40:05 +0100

    Dummy db migrations update.

--- dummy/db/schema.rb
+++ dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022163321) do
+ActiveRecord::Schema.define(version: 20151029170707) do
 
   create_table "areas", force: :cascade do |t|
     t.string   "ancestry",       limit: 255
