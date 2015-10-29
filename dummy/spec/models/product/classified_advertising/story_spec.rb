60a8ae335d9e728ad5ddabd6f34dcc7740739c0d
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-10-29 18:37:46 +0100

    refs #5 Removes all specs.

--- dummy/spec/models/product/classified_advertising/story_spec.rb
+++ dummy/spec/models/product/classified_advertising/story_spec.rb
@@ -1,41 +1,4 @@
 require 'spec_helper'
 
 describe Product::ClassifiedAdvertisement::Story do
-  describe '#tasks_attributes=' do
-    it 'creates, updates and destroys tasks' do
-      project = FactoryGirl.create(:classified_advertisement_project_with_story)
-      subject = project.stories.first
-      
-      # create
-      subject.attributes = {
-        tasks_attributes: {
-          '0' => {
-            name: 'Task 1', text: 'Dummy', 
-            vacancies_attributes: {
-              '0' => {name: 'Vacancy CA 1', text: 'Dummy', _destroy: '0'}
-            }
-          }
-        }
-      }
-      subject.setup_tasks; task = subject.tasks.first
-      
-      task.new_record?.should(be_falsey)
-      task.vacancies.first.is_a?(Product::ClassifiedAdvertisement::Vacancy)
-  
-      task_attributes = { id: task.id.clone, name: 'Task 1.1', text: 'Dummy 2' }
-      
-      # update
-      subject.update_attributes tasks_attributes: { '0' => task_attributes }; task.reload
-      
-      subject.tasks.count.should == 1
-      subject.tasks.first.vacancies.count.should == 1
-      task_attributes.each {|attribute, value| task.send(attribute).should == value }
-      
-      # destroy
-      subject.update_attributes tasks_attributes: { '0' => task_attributes.merge(_destroy: '1') }
-      
-      Task.count.should == 0
-      Vacancy.count.should == 0
-    end
-  end
 end
